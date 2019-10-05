80736edda950bb992504e17de29bd39c1e6e55e4
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2019-10-05 17:39:03 +0700

    scripts fix and third scene

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -14,4 +14,11 @@ public class Goal : MonoBehaviour {
 		
 	}
 
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.GetComponent<PlayerController>() != null) {
+            GetComponent<GameController>().TryGoToNextLevel();
+        }
+    }
+
+
 }
