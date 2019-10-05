03d0700b38d30e73704bbe3bf5222dce08d0db5b
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2019-10-05 13:37:24 +0700

    moving platform

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+    public float vSpeed = 1;
+    public float hSpeed = 1;
+
+    Rigidbody2D rgbd;
+
+    // Use this for initialization
+    void Start () {
+        rgbd = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rgbd.velocity = new Vector2(hSpeed, vSpeed);
+        //transform.position = transform.position + new Vector3(hSpeed, vSpeed, 0);
+	}
+}
