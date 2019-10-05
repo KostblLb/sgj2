36db0747bd165a48465fc17ad3dd735d0ee7847c
KostblLb <kostya19494@gmail.com>
2019-10-05 20:14:43 +0700

    mirror

---
+++ Assets/Mirror.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    public bool IsFlipped = false;
+    Camera sceneCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
